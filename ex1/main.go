6069bcba8cb33d3f47aebc619650b320eb5bafd8
Raksa523 <dusas@mail.ru>
2019-11-18 16:50:39 +0600

    ex1

--- ex1/main.go
+++ ex1/main.go
@@ -6,37 +6,53 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
-	csvFileName := flag.String("csv", "problems.csv", "a csv file description, format(question, answer)")
+	csvFileName := flag.String("csv", "problems.csv", "csv file with format \"problem, answer\"")
+	timeLimit := flag.Int("limit", 30, "the time limit for the quiz in seconds")
 	flag.Parse()
-	_ = csvFileName
-
 	file, err := os.Open(*csvFileName)
 	if err != nil {
-		exit(fmt.Sprintf("Failed to open the CSV file: %s", *csvFileName))
+		Exit(fmt.Sprintf("Failed to open file: %s\n", *csvFileName))
+	}
+	read := csv.NewReader(file)
+	lines, err := read.ReadAll()
+	problems := ParseProblems(lines)
+
+	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
+
+	correct := 0
+
+problemloop:
+	for i, p := range problems {
+		fmt.Printf("Problem #%d: %s = ", i+1, p.q)
+		answerCh := make(chan string)
+		go func() {
+			var answer string
+			fmt.Scanf("%s\n", &answer)
+			answerCh <- answer
+		}()
+		select {
+		case <-timer.C:
+			//fmt.Printf("\nCorrect answer %d out of %d: ", correct, len(problems))
+			fmt.Println()
+			//return
+			break problemloop
+		case answer := <-answerCh:
+			if answer == p.a {
+				correct++
+			}
+		}
+
 	}
 
-	r := csv.NewReader(file)
-	lines, err := r.ReadAll()
-	if err != nil {
-		exit("Failed to parse the provided CSV file")
-	}
-	problems := parseLines(lines)
-	correct := 0
-	for i, p := range problems {
-		fmt.Printf("Problem #%d: %s = \n", i+1, p.q)
-		var answer string
-		fmt.Scanf("%s\n", &answer)
-		if answer == p.a {
-			correct++
-		}
-	}
-	fmt.Printf("You scored %d out of %d\n", correct, len(problems))
+	fmt.Printf("Correct answer %d out of %d: ", correct, len(problems))
+
 }
 
-func parseLines(lines [][]string) []problem {
+func ParseProblems(lines [][]string) []problem {
 	ret := make([]problem, len(lines))
 	for i, line := range lines {
 		ret[i] = problem{
@@ -47,12 +63,12 @@ func parseLines(lines [][]string) []problem {
 	return ret
 }
 
+func Exit(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
+}
+
 type problem struct {
 	q string
 	a string
-}
-
-func exit(msg string) {
-	fmt.Printf(msg)
-	os.Exit(1)
 }
